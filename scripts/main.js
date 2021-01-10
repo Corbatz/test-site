3585587fad0c73170d88cb5fd809d088ef22104b
Corbatz <peterq171@gmail.com>
2021-01-09 23:04:32 -0600

    Hellow world script removed, image changer script added (with a new image called nomessirve), personalized welcome message added

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/hqdefault.jpg') {
+      myImage.setAttribute('src','images/nomessirve.jpg');
+    } else {
+      myImage.setAttribute('src','images/hqdefault.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Por favor ingresa tu nombre.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Messi es el mejor, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  }  
+
+myButton.onclick = function(){
+    setUserName();
+}
